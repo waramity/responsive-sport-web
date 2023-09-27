6ca0d765ed14e7a41ebf0ec528624783212f1b14
unknown <waramity.re@gmail.com>
2023-09-27 08:17:07 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const mainData = {
+  ATHLETES: {
+    CONNECTION: "Connect with coaches directly and view their profiles.",
+    COLLABORATION:
+      "Work with other student athletes to increase your visibility by sharing and liking videos.",
+    GROWTH:
+      "Access resources and tools to improve as a student athlete, including training classes and tutor sessions.",
+  },
+  PLAYERS: {
+    CONNECTION:
+      "Connect with talented athletes and watch their video showreels.",
+    COLLABORATION: "Collaborate with recruiters to find talented athletes.",
+    GROWTH: "Save time and recruit the right athletes for your team.",
+  },
+};
+
+function createSections(data, containerSelector) {
+  const mainContainer = document.querySelector(containerSelector);
+
+  let index = 1;
+  for (const category in data) {
+    const section = document.createElement("div");
+    section.className = `section-${index}`;
+    mainContainer.appendChild(section);
+
+    const header = document.createElement("h2");
+    header.textContent = category;
+    section.appendChild(header);
+
+    let indexSubCategory = 1;
+    for (const subcategory in data[category]) {
+      const indexHeader = document.createElement("span");
+      indexHeader.textContent = indexSubCategory;
+
+      const p = document.createElement("p");
+      p.textContent = data[category][subcategory];
+
+      p.insertBefore(indexHeader, p.firstChild);
+      section.appendChild(p);
+      indexSubCategory++;
+    }
+    index++;
+  }
+}
+
+createSections(mainData, ".container");
